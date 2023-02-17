86e23ab56de1e6fcfce895cf548413a7b7988011
Mateusz Wala <mateusz.wala2734@gmail.com>
2023-02-17 15:33:54 +0100

    fix: save files on desktop

--- tools/compressor.py
+++ tools/compressor.py
@@ -5,22 +5,21 @@ from pypdf.errors import PdfReadError
 
 from toolABC import Tool
 
+
 class Compressor(Tool):
     def __init__(self, path):
         self.execute(path)
 
-
     def execute(self, path):
-        print(path)   
-        print(os.getcwd())     
         try:
-            directory = os.path.dirname(path)
+            DESKTOP = os.path.join(os.path.join(
+                os.environ['USERPROFILE']), 'Desktop')
             filename = os.path.basename(path).split(".")[0]
-            out = os.path.join(directory,f"{filename}-compressed.pdf")
-            
+            out = os.path.join(DESKTOP, f"{filename}-compressed.pdf")
+
             reader = PdfReader(path)
             writer = PdfWriter()
-            
+
             for page in reader.pages:
                 page.compress_content_streams()
                 writer.add_page(page)
