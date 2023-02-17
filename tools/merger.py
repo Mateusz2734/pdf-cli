c951e5e791a5bf5e766bae0a4282bfdf20e1be7e
Mateusz Wala <mateusz.wala2734@gmail.com>
2023-02-17 20:15:18 +0100

    fix: Add docs

--- tools/merger.py
+++ tools/merger.py
@@ -1,3 +1,4 @@
+from typing import List
 import os
 
 from pypdf import PdfReader, PdfWriter
@@ -10,7 +11,14 @@ class Merger(Tool):
     def __init__(self, paths):
         self.execute(paths)
 
-    def execute(self, paths):
+    def execute(self, paths: List[str]):
+        """
+        It takes a list of paths to PDF files, merges them into a single PDF file, and saves the merged
+        PDF file to the desktop
+        
+        :param paths: A list of paths to the PDFs you want to merge
+        :type paths: List[str]
+        """
         DESKTOP = os.path.join(os.path.join(
             os.environ['USERPROFILE']), 'Desktop')
         filename = os.path.basename(paths[0]).split(".")[0]
@@ -25,6 +33,15 @@ class Merger(Tool):
             writer.write(file)
 
     def add_to_writer(self, path: str, writer: PdfWriter):
+        """
+        It takes a path to a PDF file, and a PdfWriter object, and adds the pages of the PDF file to the
+        PdfWriter object
+        
+        :param path: str - the path to the PDF file
+        :type path: str
+        :param writer: PdfWriter
+        :type writer: PdfWriter
+        """
         try:
             reader = PdfReader(path)
             for page in reader.pages:
