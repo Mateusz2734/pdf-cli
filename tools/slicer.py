82ed511360a966275ad4d9f33163006ef06731dc
Mateusz Wala <mateusz.wala2734@gmail.com>
2023-02-20 10:38:39 +0100

    fix: append path before importing module

--- tools/slicer.py
+++ tools/slicer.py
@@ -1,35 +1,35 @@
-from typing import List
 import os
 import sys
+sys.path.append('../functions')
 
-from pypdf import PdfReader, PdfWriter, PageRange
+from pypdf import PdfReader, PdfWriter
 from pypdf.errors import PdfReadError
 
 from toolABC import Tool
 from slicetools import create_pageranges
 
-sys.path.append('../functions')
-
 
 class Slicer(Tool):
-    def __init__(self, path: str, slices: List[PageRange]) -> None:
-        slices = create_pageranges(slices)
+    def __init__(self, path: str, slices: str) -> None:
         self.execute(path, slices)
 
-    def execute(self, path: str, slices: List[PageRange]):
+    def execute(self, path: str, slices: str):
         """
         It takes a PDF file and a list of page ranges, and writes a new PDF file with only the pages in
         the ranges
-        
+
         :param path: Path to the PDF file
         :type path: str
         :param slices: The page ranges to extract
-        :type slices: List[PageRange]
+        :type slices: str
         """
         DESKTOP = os.path.join(os.path.join(
             os.environ['USERPROFILE']), 'Desktop')
         filename = os.path.basename(path).split(".")[0]
         out = os.path.join(DESKTOP, f"{filename}-sliced.pdf")
+
+        slices = create_pageranges(slices)
+
         writer = PdfWriter()
         try:
             reader = PdfReader(path)
