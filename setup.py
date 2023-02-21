9759beee1beebb093f92db1a28904cd8ff428d53
Mateusz Wala <mateusz.wala2734@gmail.com>
2023-02-21 12:18:51 +0100

    Add setup file

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name="pdf-cli",
+    version="1.0",
+    packages=["commands", "functions", "tools"],
+    include_package_data=True,
+    install_requires=["pypdf==3.4.0",
+                      "click==8.1.3",
+                      "colorama"],
+    entry_points="""
+        [console_scripts]
+        pdf=main:cli
+    """,
+)
