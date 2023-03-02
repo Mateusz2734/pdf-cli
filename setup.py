e84f8f3bea8d7c5f0b8caa16636ebb694abb242a
Mateusz Wala <mateusz.wala2734@gmail.com>
2023-03-02 23:05:35 +0100

    Update setup file to enable normal install

--- setup.py
+++ setup.py
@@ -8,6 +8,7 @@ setup(
     install_requires=["pypdf==3.4.0",
                       "click==8.1.3",
                       "colorama"],
+    py_modules=["main"],
     entry_points="""
         [console_scripts]
         pdf=main:cli
